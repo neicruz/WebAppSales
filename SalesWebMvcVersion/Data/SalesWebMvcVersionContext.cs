7f6407a030c3e9f2441887bf24b477d4b0eff074
neicruz <nei.cruz@hotmail.com>
2022-03-21 23:07:31 -0300

    Project IN fase of db created

---
+++ SalesWebMvcVersion/Data/SalesWebMvcVersionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcVersion.Models;
+
+namespace SalesWebMvcVersion.Data
+{
+    public class SalesWebMvcVersionContext : DbContext
+    {
+        public SalesWebMvcVersionContext (DbContextOptions<SalesWebMvcVersionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcVersion.Models.Department> Department { get; set; }
+    }
+}
---
+++ SalesWebMvcVersion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMvcVersion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
