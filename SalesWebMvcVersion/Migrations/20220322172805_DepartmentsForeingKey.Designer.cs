7d3dba10ac874ef73da1cffcf1a8f5d09619022e
neicruz <nei.cruz@hotmail.com>
2022-03-22 14:32:55 -0300

    Foreing key not null

--- SalesWebMvcVersion/Migrations/20220322172805_DepartmentsForeingKey.Designer.cs
+++ SalesWebMvcVersion/Migrations/20220322172805_DepartmentsForeingKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMvcVersion.Data;
 
 namespace SalesWebMvcVersion.Migrations
 {
     [DbContext(typeof(SalesWebMvcVersionContext))]
-    partial class SalesWebMvcVersionContextModelSnapshot : ModelSnapshot
+    [Migration("20220322172805_DepartmentsForeingKey")]
+    partial class DepartmentsForeingKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
