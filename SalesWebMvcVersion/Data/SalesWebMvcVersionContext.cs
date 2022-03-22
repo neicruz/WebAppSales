88d433eb4a998cefe8f863fed6363d6a3843a471
neicruz <nei.cruz@hotmail.com>
2022-03-22 01:19:38 -0300

    Other Entities and second migration

--- SalesWebMvcVersion/Data/SalesWebMvcVersionContext.cs
+++ SalesWebMvcVersion/Data/SalesWebMvcVersionContext.cs
@@ -14,6 +14,8 @@ namespace SalesWebMvcVersion.Data
         {
         }
 
-        public DbSet<SalesWebMvcVersion.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord{ get; set; }
     }
 }
