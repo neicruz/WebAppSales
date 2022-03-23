aab43f5f68a0ee10f97b21074b0423dcaef0fe33
neicruz <nei.cruz@hotmail.com>
2022-03-23 17:29:54 -0300

    Exceptions delete

---
+++ SalesWebMvcVersion/Services/Exceptions/IntegrityExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvcVersion.Services.Exceptions
+{
+    public class IntegrityExceptions : ApplicationException
+    {
+        public IntegrityExceptions(string message) : base(message)
+        {
+
+        }
+    }
+}
